c68e9a1b932ae57a5a8e8cb5f6de9f033c91288a
AcePatterson <acepttrsn@gmail.com>
2024-02-29 04:54:58 -0800

    first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Scene from './scene'
+
+export default async function HomePage() {
+  return (
+    <>
+      <div className="select-none bg-zinc-700">
+        <Scene />
+      </div>
+    </>
+  )
+}
