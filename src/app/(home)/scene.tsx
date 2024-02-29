032f0151ed27f6a154b5cff51f6c5fc750dae4fe
AcePatterson <acepttrsn@gmail.com>
2024-02-29 06:34:51 -0800

    fix: detect action for raycaster

--- src/app/(home)/scene.tsx
+++ src/app/(home)/scene.tsx
@@ -1,20 +1,14 @@
 'use client'
 import Paranoma from '@/components/Paranoma'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload } from '@react-three/drei'
+import { Preload } from '@react-three/drei'
 import { Suspense } from 'react'
-import { useAction } from '@/store/useAction'
 
 export default function Scene() {
-  const { action } = useAction()
   return (
     <Suspense fallback={null}>
       <Canvas camera={{ fov: 80 }}>
         <Paranoma />
-        <OrbitControls
-          maxDistance={140}
-          enabled={action === 'none'}
-        />
         <Preload all />
       </Canvas>
     </Suspense>
--- src/components/Paranoma/index.tsx
+++ src/components/Paranoma/index.tsx
@@ -1,20 +1,35 @@
-import { BackSide } from 'three/src/Three.js'
-import { useTexture } from '@react-three/drei'
+import { BackSide, DoubleSide } from 'three/src/Three.js'
+import { OrbitControls, useTexture } from '@react-three/drei'
+import { useAction } from '@/store/useAction'
 
 export default function Paranoma() {
   const texture = useTexture('/paranoma.webp')
+  const { action } = useAction()
 
   return (
     <group>
-      <mesh
-        onPointerDown={(e) => {
-          console.log(e.point.x, e.point.y, e.point.z)
-        }}>
+      <OrbitControls
+        maxDistance={140}
+        enabled={action === 'none'}
+      />
+      <mesh>
         <sphereGeometry args={[150, 100, 100]} />
         <meshBasicMaterial
           map={texture}
           side={BackSide}
           toneMapped={false}
+        />
+      </mesh>
+      <mesh
+        onPointerDown={(e) => {
+          if (action !== 'none' && e.point.x) console.log(e.point.x, e.point.y, e.point.z)
+        }}>
+        <sphereGeometry args={[140, 100, 100]} />
+        <meshBasicMaterial
+          side={DoubleSide}
+          transparent
+          opacity={0}
+          color={'black'}
         />
       </mesh>
     </group>
